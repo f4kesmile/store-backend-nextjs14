51ba1f6f174f7faa5481bafe693ddad73dda091c
Dhimm <66907668+f4kesmile@users.noreply.github.com>
2025-06-20 15:13:47 +0700

    Update route.ts

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -3,7 +3,7 @@ import midtransClient from "midtrans-client";
 import db from "@/lib/db";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "http://localhost:3001", // Ganti dengan domain frontend Anda
+  "Access-Control-Allow-Origin": "https://hedigas-store.netlify.app", // Ganti dengan domain frontend Anda
   "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
